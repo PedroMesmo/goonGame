f08186c2c8fdf6e8454f8f5d29bd6b69df24437a
Pedro <pedropinguim65@gmail.com>
2026-09-25 19:23:22 -0300

    Refactor Goon class attributes and methods

--- gooner.py
+++ gooner.py
@@ -1,10 +1,12 @@
 class Goon:
     def __init__(self, 
                 sanidade,
+                frigidez,
                 energia,
                 goonadas,
                 genero,
                 religiao,
+                waifu,
                 furry,
                 femboy,
                 mago,
@@ -12,25 +14,31 @@ class Goon:
                 ):
         
         self.sanidade = sanidade
+        self.frigidez = frigidez
         self.energia = energia
         self.goonadas = goonadas
+        self.mulher = genero
         self.religiao = religiao
+        self.waifu = waifu
         self.furry = furry
         self.femboy = femboy
         self.mago = mago
-        self.virgem = virgem
-        self.mulher = genero
-        
+        self.virgem = virgem        
 
     def gozar(self):
         self.goonadas =+ 1
         self.sanidade = self.sanidade - 0.2 * self.sanidade
 
+
     def fapear(self):
-        self.energia = self.energia - 20
+        self.frigidez = self.frigidez - 20
+        self.sanidade = self.sanidade - 20
+        self.energia = self.energia - 5
     
     def exibir_status(self):
-        print(f'Energia: {self.energia}\n',
+        print(f'Frigidez: {self.frigidez}\n',
               f'Sanidade: {self.sanidade}\n',
               f'Goonadas: {self.goonadas}\n')
+
+        
 

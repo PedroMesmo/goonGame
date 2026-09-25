d4c1e33e734bc86a86d55cd05129d2508532a532
Pedro <pedropinguim65@gmail.com>
2026-09-25 19:22:25 -0300

    Refactor character creation and input validation

--- teste.py
+++ teste.py
@@ -1,22 +1,75 @@
-def perguntar_opcao(pergunta, opcoes):
-    while True:
-        resposta = input(pergunta)
+from gooner import Goon
 
-        if resposta in opcoes:
-            return resposta
+def criar_personagem():
 
-        print("Entrada inválida. Tente novamente.")
+    # Função para permitir somente respostas válidas perante a suprema corte
+    def questao(pergunta, opcoes):
+        while True:
+            resposta = input(pergunta).lower()
 
-nome = input("Qual seu nome? ")
+            if resposta in opcoes:
+                return resposta
 
-sexo = perguntar_opcao(
-    "Digite M ou F: ",
-    ["M", "F"]
-)
+            print("Comando descabível")
 
-tipo = perguntar_opcao(
-    "Digite A, B ou C: ",
-    ["A", "B", "C"]
-)
+    gooner = Goon(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
-print(nome, sexo, tipo)
+    # Definindo o gênero
+    mulher = questao(
+        'Mulher(trans, btw)\n[S]Sim    [N]Não\n',
+        ['s', 'n']
+    )
+
+    if mulher == 's':
+        gooner.mulher = True
+    else:
+        gooner.mulher = False
+
+    # Definindo a religião
+    religiao = questao(
+        'Religião\n'
+        '[1] Cristianismo\n'
+        '[2] Judaísmo\n'
+        '[3] Islamismo\n'
+        '[4] Budismo\n'
+        '[5] Satanismo\n'
+        '[6] Lei Felca\n'
+        '[7] Outro\n',
+        ['1', '2', '3', '4', '5', '6', '7']
+    )
+
+    if religiao == '1':
+        gooner.religiao = 'cristianismo'
+    elif religiao == '2':
+        gooner.religiao = 'judaismo'
+    elif religiao == '3':
+        gooner.religiao = 'islamismo'
+    elif religiao == '4':
+        gooner.religiao = 'budismo'
+    elif religiao == '5':
+        gooner.religiao = 'satanismo'
+    elif religiao == '6':
+        gooner.religiao = 'Lei Felca'
+    else:
+        gooner.religiao = input(
+            'Certo diferentão, nos conte qual a sua religião ultra nichada: '
+        )
+
+    # Femboy
+    femboy = questao(
+        'Femboy\n[S]Sim    [N]Não\n',
+        ['s', 'n']
+    )
+    gooner.femboy = femboy == 's'
+
+    # Furry
+    furry = questao(
+        'Furry\n[S]Sim    [N]Não\n',
+        ['s', 'n']
+    )
+    gooner.furry = furry == 's'
+
+    gooner.exibir_status()
+
+
+criar_personagem()
